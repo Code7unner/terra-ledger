05ecd07ff7bf6ceab8c7a9be0b06ddb9326c424a
mathn00b <maxim.cherbadzhi@shelterzoom.com>
2026-04-06 02:32:38 +0500

    Update icon

--- web/src/components/TopBar/TopBar.tsx
+++ web/src/components/TopBar/TopBar.tsx
@@ -1,11 +1,15 @@
 import { NavLink } from 'react-router-dom'
 import { WalletButton } from '../WalletButton/WalletButton'
+import leafIcon from '../../../favicon.svg'
 import styles from './TopBar.module.css'
 
 export function TopBar() {
   return (
     <header className={styles.topbar}>
-      <NavLink to="/" className={styles.logo}>TerraLedger</NavLink>
+      <NavLink to="/" className={styles.logo}>
+        <img src={leafIcon} className={styles.logoIcon} alt="" aria-hidden="true" />
+        TerraLedger
+      </NavLink>
       <div className={styles.right}>
         <WalletButton />
       </div>
